1cfa28b3993f2ffb680da522755d8233ba27433e
junlico <junl14@uci.edu>
2019-07-09 23:00:39 -0700

    Updates

---
+++ precache-manifest.4fe6a3f6626a7bc6c6233f7111c730c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64af8e092020520851b89746dc8bf59a",
+    "url": "/warship-girl/index.html"
+  },
+  {
+    "revision": "d02f6effe6795b1d0a96",
+    "url": "/warship-girl/static/css/main.c616717b.chunk.css"
+  },
+  {
+    "revision": "5e8725d9638589ab05ea",
+    "url": "/warship-girl/static/js/2.2a84f464.chunk.js"
+  },
+  {
+    "revision": "d02f6effe6795b1d0a96",
+    "url": "/warship-girl/static/js/main.4903e496.chunk.js"
+  },
+  {
+    "revision": "27cbc47b21b21ddc6eab",
+    "url": "/warship-girl/static/js/runtime~main.ab4cd525.js"
+  }
+]);
